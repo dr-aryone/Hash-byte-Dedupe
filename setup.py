619a6c9d4a195629a699efa1ff92cbc9d3a03345
Nathan Tamez <jamestamezn@gmail.com>
2019-01-16 07:51:04 +0000

    Commit 3
    move the main program to a sub dir

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name='DataDeDup',
-    version='0.0.1',
+    name="Hash-byte-Dedupe",
+    version="0.0.1",
+    author="Nathan Tamez",
+    author_email="jamestamezn@gmail.com",
+    description="A simple project that uses matching hash to find exact maches of files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NatoNathan/Hash-byte-Dedupe",
     py_modules=setuptools.find_packages(),
     install_requires=[
         'Click',
@@ -12,4 +21,9 @@ setuptools.setup(
         [console_scripts]
         DataDeDup=DataDeDup:cli
     ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
